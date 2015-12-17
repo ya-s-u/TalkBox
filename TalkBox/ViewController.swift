d8251f6a02da0170b4a751ff7c5140e8a1931957
Yasuaki Goto <yasu1003@gmail.com>
2015-12-17 10:07:24 +0900

    Add Xcode project

---
+++ TalkBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TalkBox
+//
+//  Created by 後藤誉昌 on 2015/12/17.
+//  Copyright © 2015年 後藤誉昌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
